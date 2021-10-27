8fc4818df79db58c7327246fadc974ab2c22a91f
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-10-27 11:03:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/luci-theme-infinityfreedom
-git clone https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
+git clone --depth=1 https://github.com/fw876/helloworld.git ./package/helloworld
 git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
